070cff02bdcc32c94bbc8d08ef0b5e4b426f348f
CARLOS MALDONADO <karloz_0986@hotmail.com>
2023-01-05 17:02:33 -0800

    Component compound

---
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -0,0 +1,34 @@
+import { createContext, useContext } from 'react';
+import { useProduct } from '../hooks/useProduct';
+import { ProductContextProps, ProductCardProps } from '../interfaces/interfaces';
+import  styles from '../styles/styles.module.css';
+
+
+export const ProductContext = createContext({} as ProductContextProps)
+const { Provider } = ProductContext;
+
+
+export const ProductCard = ({ children, product }: ProductCardProps) => {
+
+  const { counter, increaseBy } = useProduct();
+    
+  return (
+    <Provider value={{
+        counter,
+        increaseBy,
+        product
+    }}>
+        <div className={ styles.productCard }>
+
+             { children }
+
+            {/* <img className={ styles.productImg } src={ noImage } alt="imagen" /> */}
+            {/*         <ProductImage img={ product.img }/>
+            <ProductTitle title={ product.title }/>
+            <ProductButtons counter={ counter} increaseBy={ increaseBy }/> */}
+
+        </div>
+    </Provider>
+
+  )
+}
