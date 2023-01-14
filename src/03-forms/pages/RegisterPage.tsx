de9a382c2fa07287c25e5625c274907ad5dca4cb
CARLOS MALDONADO <karloz_0986@hotmail.com>
2023-01-13 16:55:52 -0800

    FormikComponents

--- src/03-forms/pages/RegisterPage.tsx
+++ src/03-forms/pages/RegisterPage.tsx
@@ -1,28 +1,22 @@
 import { ChangeEvent, FormEvent, useState } from 'react';
+import { useForm } from '../hooks/useForm';
 import '../styles/styles.css';
 
 export const RegisterPage = () => {
 
-    const [registerData, setRegisterData] = useState({
+    const { formData, onChange, resetForm , isValidEmail ,name, email, password1, password2 } = useForm({
         name: '',
         email: '',
         password1: '',
         password2: '',
     });
 
-    const { name, email, password1, password2 } = registerData;
-
-    const onChange = ( event: ChangeEvent<HTMLInputElement> ) => {
-       setRegisterData( prev => ({
-          ...prev,
-          [ event.target.name ] : event.target.value
-       }))
-    }
 
     const onSubmit = ( event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        console.log({ registerData })
+        console.log( formData );
     }
+
 
   return (
     <div>
@@ -34,15 +28,19 @@ export const RegisterPage = () => {
                 value={ name }
                 name='name'
                 onChange={ onChange }
+                className={ `${ name.trim().length <= 0 && 'has-error' }` }
             
             />
+            { name.trim().length <= 0 && <span>Este campo es necesario</span> }
             
             <input type="email"
                 placeholder="Email"
                 value={ email }
                 name='email'
+                className={ `${ !isValidEmail( email ) && 'has-error' }` }
                 onChange={ onChange }
             />
+            { !isValidEmail( email ) && <span>Email no es valido</span>}
 
             
             <input type="password"
@@ -51,6 +49,8 @@ export const RegisterPage = () => {
                 name='password1'
                 onChange={ onChange }
             />
+            { password1.trim().length <= 0 && <span>Este campo es necesario</span> }
+            { password1.trim().length < 6 && password1.trim().length > 0 && <span>La contrasena tiene que tener 6 caracteres</span>}
 
             <input type="password"
                 placeholder="Repeat Password"
@@ -58,8 +58,11 @@ export const RegisterPage = () => {
                 name='password2'
                 onChange={ onChange }
             />
+            { password2.trim().length <= 0 && <span>Este campo es necesario</span> }
+            { password2.trim().length > 0 && password1 !== password2 && <span>Las contrasenas deben de ser iguales</span>}
 
             <button type="submit">Create</button>
+            <button type='button' onClick={ resetForm }>Reset Form</button>
             
         </form>
     </div>
--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,5 +1,8 @@
 import { BrowserRouter } from 'react-router-dom';
 import { Routes, Route, NavLink, Navigate } from 'react-router-dom';
+import { FormikBasicPage } from '../03-forms/pages/FormikBasicPage';
+import { FormikComponents } from '../03-forms/pages/FormikComponents';
+import { FormikYupPage } from '../03-forms/pages/FormikYupPage';
 import { RegisterPage } from '../03-forms/pages/RegisterPage';
 
 
@@ -17,7 +20,13 @@ export const Navigation = () => {
                             <NavLink to="/register" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Register Page</NavLink>
                         </li>
                         <li>
-                            <NavLink to="/about" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>About</NavLink>
+                            <NavLink to="/formik-basic" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Formik - Basic</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/formik-yup" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Formik - Yup</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/formik-components" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Formik - Components</NavLink>
                         </li>
                         <li>
                             <NavLink to="/users" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Users</NavLink>
@@ -27,7 +36,9 @@ export const Navigation = () => {
 
 
                 <Routes>
-                    <Route path="about" element={ <h1>About Page</h1> } />
+                    <Route path="/formik-basic" element={ <FormikBasicPage/> } />
+                    <Route path='/formik-yup' element={ <FormikYupPage/>}/>
+                    <Route path='/formik-components' element={ <FormikComponents/>}/>
                     <Route path="users" element={ <h1>Users Page</h1> } />
                     <Route path="home" element={ <h1>Home Page</h1> } />
                     <Route path='/register' element={ <RegisterPage/> } />
